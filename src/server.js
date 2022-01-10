9490a7c4609b45b37b7a0f15951b126e70f29422
juniorjwj <junio2063@gmail.com>
2022-01-10 15:06:13 -0300

    Estrutura base
    Estrutura base

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const server = express()
+const mongoose = require('mongoose')
+const route = require('./route')
+const path = require('path')
+const { resolve } = require('path')
+
+server.set('view engine', 'ejs')
+server.use(express.static("publics"))
+server.set('views', path.join(__dirname, 'views'))
+server.use(express.urlencoded({ extended: true}))
+
+
+server.use(route)
+server.listen(3000, () => console.log('server on'))
